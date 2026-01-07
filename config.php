4cfa4a933ae7c89606f3a26bfbd130e765bb16a2
EmiVatavu <emilianvatavu@gmail.com>
2026-01-07 15:59:27 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "transport";
+
+$host = "sql111.infinityfree.com"; // hostu
+$user = "if0_40382618"; // Useru
+$pass = "parolanuecomuna"; // parola
+$db = "if0_40382618_transport"; 
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
@@ -11,4 +11,14 @@ if (!$conn) {
 }
 
 session_start();
+
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+function checkRole($requiredRole) {
+    if (!isset($_SESSION['role']) || $_SESSION['role'] !== $requiredRole) {
+        die("Acces interzis! Nu ai drepturile necesare.");
+    }
+}
 ?>
