167804f01054f4b215543335dfb4773ac8d3666c
EmiVatavu <emilianvatavu@gmail.com>
2026-01-07 16:01:38 +0200

    Update delete_vehicul.php

--- delete_vehicul.php
+++ delete_vehicul.php
@@ -1,6 +1,18 @@
 <?php
 include 'config.php';
-$id = $_GET['id'];
+checkRole('admin');
 
-mysqli_query($conn, "DELETE FROM vehicule WHERE id=$id");
+if (isset($_GET['id']) && isset($_GET['token'])) {
+    if ($_GET['token'] !== $_SESSION['csrf_token']) {
+        die("Eroare securitate. Link invalid.");
+    }
+
+    $id = $_GET['id'];
+    $stmt = mysqli_prepare($conn, "DELETE FROM vehicule WHERE id=?");
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+}
+
 header("Location: vehicule.php");
+exit;
+?>
